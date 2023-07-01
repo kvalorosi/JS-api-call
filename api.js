302847adb8737efe04b4d06ed451239532f79593
Kaytlin Valorosi <kvalorosi@yahoo.com>
2023-07-01 15:13:42 -0700

    somework

--- api.js
+++ api.js
@@ -1,18 +1,20 @@
-//const apiUrl = "https://pokeapi.co/api/v2/ability/";
+const apiUrl = "https://pokeapi.co/api/v2/ability/";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const nameElement = document.querySelector(".name h1");
 const spriteElement = document.querySelector(".sprite");
 
-async function findPoke(name){
-    const response = await fetch("https://pokeapi.co/api/v2/ability/");
+async function findPokemon(name){
+    const response = await fetch(apiUrl + name);
     var data = await response.json();
 
     console.log(data)
+    nameElement.textContent = data.name;
+    spriteElement.src = data.sprites.front_default;
     
 }
-searchBtn.addEventListener("click", ()=>{
-    findPoke(searchBox.value);
+searchBtn.addEventListener("click", () => {
+    findPokemon(searchBox.value);
 })
-findPoke("ability-name");
+findPokemon(name);
